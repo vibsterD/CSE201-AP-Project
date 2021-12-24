3e021ecd8a9cb3e743d827f853526d5dcea5b71e
Vibhu Dubey <vibhudubey97.a@gmail.com>
2021-12-24 22:42:23 +0530

    Class skeleton part-1

---
+++ Will-Hero/src/main/java/com/ap43iiitd/willhero/Player.java
@@ -0,0 +1,55 @@
+package com.ap43iiitd.willhero;
+
+public class Player extends GameObject {
+    private int coins;
+    private Helmet helmet;
+    private Boolean respawned;
+    private int location;
+    private Boolean is_eliminated;
+    private Game game;
+    private Boolean has_won;
+    private Boolean dash_collision;
+    private int jump_length;
+    private int jump_frequency;
+
+    public Player() {
+        this.respawned = false;
+        this.is_eliminated = false;
+        this.has_won = false;
+        this.dash_collision = false;
+    }
+
+    public Player(Boolean respawned, Position position, int location, Weapon weapon) {
+        this.respawned = false;
+        this.is_eliminated = false;
+        this.has_won = false;
+        this.dash_collision = false;
+    }
+
+    public void jump() {}
+    private void attack() {}
+    public void dash() {}
+    public void updateCoins(int delta) {}
+    public int getCoins() {
+        return this.coins;
+    }
+    public void updateWeapon(Weapon weapon) {}
+    private void upgradeWeapon() {}
+    public void eliminate() {}
+    public Boolean hasRespawned() {
+        return true;
+    }
+    public Boolean getDashCollision() {
+        return true;
+    }
+    public Boolean hasWon() {
+        return this.has_won;
+    }
+    public Boolean eliminated() {
+        return this.is_eliminated;
+    }
+    public void setHasWon() {
+        this.has_won = true;
+    }
+
+}
---
+++ Will-Hero/src/main/java/com/ap43iiitd/willhero/TNT.java
@@ -0,0 +1,13 @@
+package com.ap43iiitd.willhero;
+
+public class TNT extends GameObject{
+    private int explosion_radius;
+
+    public TNT() {}
+
+    public void activate() {}
+    private void explode() {}
+
+    @Override
+    public void collide(GameObject o1) {};
+}
