faafe811fef04e031f7310b019397c014cffc460
Anindya <anindya20024@iiitd.ac.in>
2021-12-25 02:25:01 +0530

    smoothening LINEAR trans

--- Will-Hero/src/main/java/com/ap43iiitd/willhero/Position.java
+++ Will-Hero/src/main/java/com/ap43iiitd/willhero/Position.java
@@ -1,13 +1,17 @@
 package com.ap43iiitd.willhero;
 
+import javafx.animation.Interpolator;
+import javafx.animation.TranslateTransition;
 import javafx.scene.image.ImageView;
+import javafx.util.Duration;
 
 import java.io.Serializable;
 
 public class Position implements Serializable {
     private double vel_x;
     private double vel_y;
-    private final double gravity = 10;
+    private final double gravity = 30;
+    private final TranslateTransition splineTranslate = new TranslateTransition(Duration.millis(1000.0/30));
 
     public Position() {
         this.vel_x = 0;
@@ -25,7 +29,11 @@ public class Position implements Serializable {
     public void updatePosition(ImageView imageView) {
 
         this.vel_y = vel_y + gravity/60;
-        imageView.setTranslateY(imageView.getTranslateY()+vel_y);
+        splineTranslate.setNode(imageView);
+        splineTranslate.setInterpolator(Interpolator.LINEAR);
+        splineTranslate.setByY(vel_y);
+        splineTranslate.play();
+        //imageView.setTranslateY(imageView.getTranslateY()+vel_y);
     }
 
     public void setVelocity(int vel_x, int vel_y) {
