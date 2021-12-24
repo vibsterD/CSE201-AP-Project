bb2e9559ff172bc906dd42edff20a39bdd37c013
Anindya <anindya20024@iiitd.ac.in>
2021-12-24 22:49:36 +0530

    new

---
+++ Will-Hero/src/main/java/com/ap43iiitd/willhero/Shuriken.java
@@ -0,0 +1,12 @@
+package com.ap43iiitd.willhero;
+
+public class Shuriken extends Weapon{
+    @Override
+    public void attack() {
+
+    }
+
+    private void throwShuriken(){
+
+    }
+}
---
+++ Will-Hero/src/main/java/com/ap43iiitd/willhero/Sword.java
@@ -0,0 +1,12 @@
+package com.ap43iiitd.willhero;
+
+public class Sword extends Weapon{
+    @Override
+    public void attack() {
+
+    }
+
+    private void meleeAttack(){
+
+    }
+}
