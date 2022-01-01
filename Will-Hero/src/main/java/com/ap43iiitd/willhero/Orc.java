174168b28a7a8ee7608fe1c11feaa839e5468831
Vibhu Dubey <vibhu20150@iiitd.ac.in>
2022-01-01 12:52:22 +0530

    Orc Fallthrough on Death

--- Will-Hero/src/main/java/com/ap43iiitd/willhero/Orc.java
+++ Will-Hero/src/main/java/com/ap43iiitd/willhero/Orc.java
@@ -11,11 +11,13 @@ public abstract class Orc extends GameObject implements Serializable {
     final static Random r1 = new Random(786);
     private final int size;
     private int hp;
+    private Boolean collidable;
 
     public Orc(int size, int hp, Position position, int res_num){
         this.size = size;
         this.hp = hp;
         this.position = position;
+        this.collidable = true;
 //        System.out.println("CREATING ORC");
         Image image = new Image("com/ap43iiitd/willhero/imageres/Sprites/Orcs/O (".concat(String.valueOf(res_num)).concat(").png"));
         this.image_fx = new ImageView(image);
@@ -42,7 +44,9 @@ public abstract class Orc extends GameObject implements Serializable {
     private void die(){
         System.out.println("ORC DIED");
 //        image_fx.setVisible(false);
-        image_fx.setFitWidth(20);
+//        image_fx.setFitWidth(20);
+//        image_fx.setVisible(false);
+        collidable = false;
     }
 
 
@@ -61,42 +65,44 @@ public abstract class Orc extends GameObject implements Serializable {
 
     @Override
     public void collide(GameObject o1) {
-        Orc orc2 = (Orc) this;
-        if (o1 instanceof Island) {
-            Island island = (Island) o1;
-            if (orc2.getImage_fx().getBoundsInParent().intersects(island.getImage_fx().getBoundsInParent())) {
-                double height = island.getImage_fx().getFitHeight() / 2;
-                if (orc2.getPosition().getVel_y() > 0) {
+        if(collidable) {
+            Orc orc2 = (Orc) this;
+            if (o1 instanceof Island) {
+                Island island = (Island) o1;
+                if (orc2.getImage_fx().getBoundsInParent().intersects(island.getImage_fx().getBoundsInParent())) {
+                    double height = island.getImage_fx().getFitHeight() / 2;
+                    if (orc2.getPosition().getVel_y() > 0) {
 //                    System.out.println("Collided with island");
-                    orc2.getPosition().setVelocity(0, -5);
+                        orc2.getPosition().setVelocity(0, -5);
+                    }
+
                 }
 
             }
 
-        }
+            if (o1 instanceof Orc) {
+                Orc orc1 = (Orc) o1;
+                if (orc1.getImage_fx().getBoundsInParent().intersects(orc2.getImage_fx().getBoundsInParent())) {
+                    double orc_min_y = orc2.getImage_fx().getBoundsInParent().getMinY();
+                    double orc1_max_y = orc1.getImage_fx().getBoundsInParent().getMaxY();
+                    double y_del_down = Math.abs(orc_min_y - orc1_max_y);
 
-        if(o1 instanceof Orc) {
-            Orc orc1 = (Orc) o1;
-            if(orc1.getImage_fx().getBoundsInParent().intersects(orc2.getImage_fx().getBoundsInParent())) {
-                double orc_min_y = orc2.getImage_fx().getBoundsInParent().getMinY();
-                double orc1_max_y = orc1.getImage_fx().getBoundsInParent().getMaxY();
-                double y_del_down = Math.abs(orc_min_y - orc1_max_y);
+                    if (y_del_down < 10.0) {
+                        orc1.getPosition().setVelocity(0, -5);
+                        return;
+                    }
 
-                if (y_del_down < 10.0) {
-                    orc1.getPosition().setVelocity(0, -5);
-                    return;
-                }
+                    // orc1 orc2
 
-                // orc1 orc2
+                    double orc_min_x = orc2.getImage_fx().getBoundsInParent().getMinX();
+                    double orc1_max_x = orc1.getImage_fx().getBoundsInParent().getMaxX();
+                    double x_del_right = Math.abs(orc_min_x - orc1_max_x);
 
-                double orc_min_x = orc2.getImage_fx().getBoundsInParent().getMinX();
-                double orc1_max_x = orc1.getImage_fx().getBoundsInParent().getMaxX();
-                double x_del_right = Math.abs(orc_min_x - orc1_max_x);
-
-                if (x_del_right < 50.0) {
+                    if (x_del_right < 50.0) {
 //                    System.out.println("COLLIDING ORC ORC");
-                    orc2.getPosition().setVelocity(50, -1);
+                        orc2.getPosition().setVelocity(50, -1);
 
+                    }
                 }
             }
         }
